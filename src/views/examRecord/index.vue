5dddc2da2f1c4ce4d5b1d564a9ff81f8cb258626
Extreme-S <1716224950@qq.com>
2021-03-15 23:47:41 +0800

    3-15

--- src/views/examRecord/index.vue
+++ src/views/examRecord/index.vue
@@ -1,6 +1,25 @@
 <template>
   <div>
-    记录管理
+    <el-card class="box-card">
+      <el-row :gutter="20">
+        <el-col :span="6">
+          <el-input v-model="input" placeholder="请输入内容"></el-input>
+        </el-col>
+
+        <el-col :span="6">
+          <el-input v-model="input" placeholder="请输入内容"></el-input>
+        </el-col>
+
+        <el-col :span="6">
+          <el-input v-model="input" placeholder="请输入内容"></el-input>
+        </el-col>
+
+        <el-col :span="3" :offset="2">
+          <el-button type="primary">查询信息</el-button>
+        </el-col>
+      </el-row>
+    </el-card>
+
   </div>
 </template>
 
